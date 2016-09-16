6c5cf951de2f19b11f46cd46f6ed1882fb9e712c
Ryan Apking <ryan.apking@gmail.com>
2016-09-16 12:15:37 -0700

    add the class Category.

--- src/Task.php
+++ src/Task.php
@@ -2,11 +2,13 @@
 class Task
 {
     private $description;
+    private $id;
 
 //Constructor
-    function __construct($description)
+    function __construct($description, $id = null)
     {
         $this->description = $description;
+        $this->id = $id;
     }
 
 //Getter and Setters
@@ -20,13 +22,32 @@ class Task
         return $this->description;
     }
 
+    function getId()
+    {
+        return $this->id;
+    }
+
 //Regular Methods
     function save()
     {
         $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+        $this->id = $GLOBALS['DB']->lastInsertId();
     }
 
 //Static Methods
+
+    static function find($search_id)
+    {
+        $found_task = null;
+        $tasks = Task::getAll();
+        foreach($tasks as $task) {
+            $task_id = $task->getId();
+            if ($task_id == $search_id) {
+                $found_task = $task;
+            }
+        }
+        return $found_task;
+    }
 
     static function getAll()
     {
@@ -34,7 +55,8 @@ class Task
         $tasks = array();
         foreach($returned_tasks as $task) {
             $description = $task['description'];
-            $new_task = new Task($description);
+            $id = $task['id'];
+            $new_task = new Task($description, $id);
             array_push($tasks, $new_task);
         }
         return $tasks;
